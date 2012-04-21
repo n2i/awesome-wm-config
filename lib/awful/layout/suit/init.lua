7ec7140e8664a918a4b4bc161069fb4f0e59d0d0
xuansamdinh <n2i.linux@gmail.com>
2012-04-21 10:36:23 +0700

    Remove lib and vicious from repo

--- lib/awful/layout/suit/init.lua
+++
@@ -1,9 +0,0 @@
-require("awful.layout.suit.max")
-require("awful.layout.suit.tile")
-require("awful.layout.suit.fair")
-require("awful.layout.suit.floating")
-require("awful.layout.suit.magnifier")
-require("awful.layout.suit.spiral")
-
---- Suits for awful
-module("awful.layout.suit")
